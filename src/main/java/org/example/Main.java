e8525a3e49075b2db82e417c7c8aac387de3fb6f
Aashif Sajah <masaashifsajah@gmail.com>
2024-12-14 01:21:39 +0530

    Added dependency

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,37 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import org.example.config.Configuration;
+import org.example.core.TicketPool;
+import org.example.logging.Logger;
+import org.example.threads.Customer;
+import org.example.threads.Vendor;
+import org.example.ui.CommandLineInterface;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Configuration config = CommandLineInterface.configureSystem();
+        TicketPool ticketPool = new TicketPool();
+
+        Thread vendor = new Thread(
+                new Vendor(ticketPool,
+                        config.getTicketReleaseRate()));
+
+        Thread customer = new Thread(new Customer(ticketPool));
+
+        vendor.start();
+        customer.start();
+
+        try
+        {
+            vendor.join();
+            customer.join();
+
+        } catch (InterruptedException e)
+        {
+            Logger.log("Main thread interrupted.");
+        }
+        Logger.log("System terminated.");
     }
 }
